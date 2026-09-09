66153b0775e601ae9b38761df84064104c488a15
Aldo Santos <aldoacesso@gmail.com>
2026-09-09 14:07:55 -0300

    feat: Product list modified
    Modified the product list, removed the category, and added stock.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 const produtos = [
-  { id: 1, nome: "Teclado Mecânico", preco: 150, categoria: "Periferico" },
-  { id: 2, nome: "Mouse Gamer", preco: 80, categoria: "Periferico" },
-  { id: 3, nome: "Monitor 24'", preco: 900, categoria: "Video" },
-  { id: 4, nome: "Headset USB", preco: 220, categoria: "Periferico" },
-  { id: 5, nome: "Placa de Vídeo", preco: 2500, categoria: "Video" },
+  { id: 1, nome: "Teclado Mecânico", preco: 150, estoque: 3 },
+  { id: 2, nome: "Mouse Gamer", preco: 80, estoque: 5 },
+  { id: 3, nome: "Monitor 24'", preco: 900, estoque: 2 },
+  { id: 4, nome: "Headset USB", preco: 220, estoque: 0 },
+  { id: 5, nome: "Placa de Vídeo", preco: 2500, estoque: 0 },
 ];
 
 const produtoPremium = produtos.find((p) => p.id === 5);
@@ -38,4 +38,3 @@ htmlLinhas += `
   `;
 
 tbody.innerHTML = htmlLinhas;
-console.log(valorTotal);
