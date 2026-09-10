5d2fbdba6f1e5fb17f468326d63f532fbf6a14b3
Aldo Santos <aldoacesso@gmail.com>
2026-09-10 10:57:27 -0300

    feat: In the array of objects,  was removed and  was added.

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
+// 1. Dados em memória
 let produtos = [
-  { id: 1, nome: "Teclado Mecânico", preco: 150, estoque: 3 },
-  { id: 2, nome: "Mouse Gamer", preco: 80, estoque: 5 },
-  { id: 3, nome: "Monitor 24'", preco: 900, estoque: 2 },
-  { id: 4, nome: "Headset USB", preco: 220, estoque: 0 },
-  { id: 5, nome: "Placa de Vídeo", preco: 2500, estoque: 5 },
+  {
+    id: 1,
+    nome: "Teclado Mecânico",
+    categoria: "Periferico",
+    preco: 150,
+    estoque: 3,
+  },
+  {
+    id: 2,
+    nome: "Mouse Gamer",
+    categoria: "Periferico",
+    preco: 80,
+    estoque: 5,
+  },
+  { id: 3, nome: "Monitor 24'", categoria: "Video", preco: 900, estoque: 2 },
+  {
+    id: 4,
+    nome: "Headset USB",
+    categoria: "Periferico",
+    preco: 220,
+    estoque: 0,
+  },
+  {
+    id: 5,
+    nome: "Placa de Vídeo",
+    categoria: "Video",
+    preco: 2500,
+    estoque: 5,
+  },
 ];
 
 const produtoPremium = produtos.find((p) => p.id === 5);
